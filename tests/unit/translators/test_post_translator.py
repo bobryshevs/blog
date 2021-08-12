f0df317984c9e14f23a4bcaa64f57ecac3752eb9
bobryshevs <wstswsb@gmail.com>
2021-08-12 13:31:32 +0300

    change iso_string in Post to datetime

--- tests/unit/translators/test_post_translator.py
+++ tests/unit/translators/test_post_translator.py
@@ -31,7 +31,7 @@ def test_to_document_valid(post_translator):
     document = {
         "text": "Text for post",
         "author": "Sviatoslav Bobryshev",
-        "date_of_creation": "2021-08-11T16:52:25.551959"
+        "date_of_creation": datetime(2021, 8, 11, 16, 52, 25, 551959)
     }
 
     result = post_translator.to_document(post)
@@ -47,8 +47,7 @@ def test_from_document_valid(post_translator):
         "_id": ObjectId('6112f2704808cefd2cf9ccfb'),
         "text": "Text from document valid",
         "author": "Sviatoslav",
-        "date_of_creation": "2021-08-11T00:41:04.327873"
-    }
+        "date_of_creation": datetime(2021, 8, 11, 0, 41, 4, 327873)}
 
     result = post_translator.from_document(document)
 
@@ -56,6 +55,4 @@ def test_from_document_valid(post_translator):
     assert document['_id'] == result.id
     assert document['text'] == result.text
     assert document['author'] == result.author
-    assert document['date_of_creation'] == result.date_of_creation.isoformat()
-    assert datetime.fromisoformat(document['date_of_creation'])\
-        == result.date_of_creation
+    assert document['date_of_creation'] == result.date_of_creation
--- translators/post_tranlator.py
+++ translators/post_tranlator.py
@@ -8,24 +8,13 @@ class PostTranslator:
         return {
             "text": post.text,
             "author": post.author,
-            "date_of_creation": post.date_of_creation.isoformat()
+            "date_of_creation": post.date_of_creation
         }
-
-    # ToDO: move this function to the PostPresenter
-    def from_dict(self, post: dict) -> Post:
-        '''
-        returns Post without m_id
-        '''
-        return Post(
-            text=post['text'],
-            author=post['author'],
-            date_of_creation=datetime.now())
 
     def from_document(self, mongo_post: dict) -> Post:
         post = Post(
             text=mongo_post['text'],
             author=mongo_post['author'],
-            date_of_creation=datetime.fromisoformat(
-                mongo_post['date_of_creation']),
+            date_of_creation=mongo_post['date_of_creation'],
             m_id=mongo_post['_id'])
         return post
