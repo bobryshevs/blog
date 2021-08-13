a113778babc675ceae7d905dba61db416184a96f
bobryshevs <wstswsb@gmail.com>
2021-08-13 14:19:07 +0300

    remove type check from repo

--- repositories/post_repository.py
+++ repositories/post_repository.py
@@ -1,10 +1,9 @@
 from datetime import datetime
 from translators.post_tranlator import PostTranslator
-from typing import Any
 from pymongo import MongoClient
 from bson import ObjectId
 from repositories.repository import Repository
-from models.post import Post
+from models import Post
 
 
 class PostRepository(Repository):
@@ -28,54 +27,31 @@ class PostRepository(Repository):
         ]
         return posts
 
-    def get_by_id(self, post_id: ObjectId) -> Any:
+    def get_by_id(self, post_id: ObjectId) -> Post:
         post = self.collection.find_one({"_id": post_id})
-        if post is None:
-            return None
-        else:
+        if post is not None:
             return self.translator.from_document(post)
+        return None
 
     def delete_by_id(self, post_id: ObjectId) -> bool:
-        if not isinstance(post_id, ObjectId) or not self.is_exists(post_id): 
-            return False
-
-        self.collection.delete_one({'_id': post_id})
-        return True
-
-    def create(self, text: str, author: str) -> ObjectId:
-        if not isinstance(text, str) or not isinstance(author, str):
-            return None
-        post = Post(text=text, author=author, date_of_creation=datetime.now())
-        created_id = self.collection.insert_one(
-                self.translator.to_document(post)).inserted_id
-        return created_id 
-
-    def update(self, post_id: ObjectId, text: str, author: str) -> Post:
-        if not isinstance(post_id,  ObjectId) or \
-                    not isinstance(text, str) or \
-                    not isinstance(author, str):
-           return None 
-
-        if not self.is_exists(post_id):
-            return None    
-        self.collection.update_one({'_id': ObjectId(post_id)},
-                                   {"$set": {"text": text, "author": author}})
-        return self.get_by_id(post_id)
-    
-
-    def is_exists(self, post_id: ObjectId) -> bool:
-        if not isinstance(post_id, ObjectId):
-            return False
-        
-        if self.get_by_id(post_id):
+        if self.exists(post_id):
+            self.collection.delete_one({'_id': post_id})
             return True
-        
         return False
 
-if __name__ == "__main__":
-    MONGO_HOST = "localhost"
-    MONGO_PORT = 27017
-    mongo_client = MongoClient(f"mongodb://{MONGO_HOST}:{MONGO_PORT}/")
+    def create(self, text: str, author: str) -> ObjectId:
+        post = Post(text, author, datetime.now())
+        created_id = self.collection.insert_one(
+            self.translator.to_document(post)).inserted_id
+        return created_id
 
-    post_repo = PostRepository(mongo_client)
-    print(post_repo.get_by_id('asd'))
+    def update(self, post: Post) -> Post:
+        if not self.exists(post.id):
+            return None
+        self.collection.update_one(
+            {'_id': post.id},
+            {"$set": {self.translator.to_document(post)}})
+        return self.get_by_id(post.id)
+
+    def exists(self, post_id: ObjectId) -> bool:
+        return self.get_by_id(post_id) is not None
