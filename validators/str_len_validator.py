d38f6df25bfaa8a8d0bd30fa6e85875681b9e9a6
bobryshevs <wstswsb@gmail.com>
2021-08-20 13:23:10 +0300

    update str_len_validator

--- validators/str_len_validator.py
+++ validators/str_len_validator.py
@@ -8,14 +8,17 @@ class StrLenValidator():
         '''[min_len, max_len] --> Including interval boundaries'''
         if not isinstance(args.get(self.key), str):
             return self.__skip()
-            
-        if min_len is None and max_len is None:
+
+        if min_len is None and max_len is None:  # (None, None)
             return True
 
-        if max_len is not None:
-            return min_len <= len(args.get(self.key)) <= max_len
+        if min_len is not None and max_len is None:  # (int, None)
+            return min_len <= len(args.get(self.key))
 
-        return min_len <= len(args.get(self.key))
+        if min_len is None and max_len is not None:  # (None, int)
+            return max_len <= len(args.get(self.key))
+
+        return min_len <= len(args.get(self.key)) <= max_len  # (int, int)
 
     def __skip(self):
         return True
