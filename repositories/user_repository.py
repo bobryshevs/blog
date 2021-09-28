51254c47057371d04b028d34248d1a289c02c61b
bobryshevs <wstswsb@gmail.com>
2021-09-28 15:00:48 +0300

    add but not tested check exists email

--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -2,6 +2,12 @@ from bson import ObjectId
 from .mongo_repository import MongoRepository
 from models import User
 
+
 class UserRepository(MongoRepository):
     def create(self, model: User) -> ObjectId:
+        if self.exists_email(model.email):
+            return None
         return super().create(model)
+
+    def exists_email(self, email: str) -> bool:
+        return self.collection.find_one({"email": email}) is not None
