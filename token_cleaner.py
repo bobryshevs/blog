a62d57a1a0a590ad1d972117137bdd5fe082bdc9
bobryshevs <wstswsb@gmail.com>
2021-10-19 11:40:58 +0300

    keep logs

---
+++ token_cleaner.py
@@ -0,0 +1,11 @@
+from repositories import UserRepository
+
+
+class TokenCleaner:
+    def __init__(self, user_repository: UserRepository) -> None:
+        self.repository: UserRepository = user_repository
+
+    def clean(self):
+    
+
+    def 
